3d04acf1fb225f42edcbf242a9afa3031a0ee4a7
Blake Hagen <blakehagen@gmail.com>
2016-04-01 21:43:42 -0600

    all endpoints added

--- server/features/fitbit/fitbit.server.routes.js
+++ server/features/fitbit/fitbit.server.routes.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const request  = require('request');
-const Promise  = require('bluebird');
-const BRequest = Promise.promisify(request, {multiArgs: true});
+const request         = require('request');
+const Promise         = require('bluebird');
+const BRequest        = Promise.promisify(request, {multiArgs: true});
+const FITBIT_BASE_URL = 'https://api.fitbit.com/1/user/-';
 
 module.exports = (app, passport) => {
 
@@ -23,19 +24,63 @@ module.exports = (app, passport) => {
       refreshToken: req.user.refreshToken
     };
 
-    let options = {
-      url: 'https://api.fitbit.com/1/user/-/activities/date/today.json',
-      method: 'GET',
-      headers: {
-        "Authorization": `Bearer ${userCredentials.accessToken}`
+    let fitbitResults  = [];
+    let fitbitRequests = [
+      {
+        url: FITBIT_BASE_URL + '/profile.json',
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${userCredentials.accessToken}`
+        }
+      },
+      {
+        url: FITBIT_BASE_URL + '/activities/date/today.json',
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${userCredentials.accessToken}`
+        }
+      },
+      {
+        url: FITBIT_BASE_URL + '/activities/steps/date/today/7d.json',
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${userCredentials.accessToken}`
+        }
+      },
+      {
+        url: FITBIT_BASE_URL + '/activities/minutesVeryActive/date/today/7d.json',
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${userCredentials.accessToken}`
+        }
+      },
+      {
+        url: FITBIT_BASE_URL + '/activities.json',
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${userCredentials.accessToken}`
+        }
       }
-    };
+    ];
 
-    BRequest(options).spread((response, body) => {
-      console.log([body]);
 
-      res.status(200).json([body]);
-    });
+    // let options = {
+    //   url: 'https://api.fitbit.com/1/user/-/activities/date/today.json',
+    //   method: 'GET',
+    //   headers: {
+    //     "Authorization": `Bearer ${userCredentials.accessToken}`
+    //   }
+    // };
 
+
+    BRequest(fitbitRequests[4])
+      .spread((response, body) => {
+        // console.log(body);
+        res.status(500).json(body);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send(error);
+      })
   });
 };
